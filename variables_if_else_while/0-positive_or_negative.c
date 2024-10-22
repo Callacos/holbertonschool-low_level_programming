4b2c5fd8770bdbb0aa4ce0ba19d3b0dd2dc86978
Callacos <callacos74@gmail.com>
2024-10-22 13:52:40 +0200

    1

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,31 +4,36 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 /* Initialize random number generator */
-srand(time(0)); /* Seed the random number generator with the current time */
+	srand(time(0)); /* Seed the random number generator with the current time */
 /* Generate a random number between -100 and 100 */
-n = (rand() % 201) - 100;
+	n = (rand() % 201) - 100;
 
 /* Print the generated number */
-printf("%d ", n);
+	printf("%d ", n);
 /* Determine if n is positive, negative, or zero */
-if (n > 0)
-{
-printf("is positive\n"); /* If n is positive */
-}
-else if (n == 0)
-{
-printf("is zero\n"); /* If n is zero */
-}
-else
-{
-printf("is negative\n"); /* If n is negative */
-}
+	if (n > 0)
+	{
+		printf("is positive\n"); /* If n is positive */
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n"); /* If n is zero */
+	}
+	else
+	{
+		printf("is negative\n"); /* If n is negative */
+	}
 
-srand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	return (0);
