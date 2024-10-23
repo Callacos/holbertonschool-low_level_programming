a473c60f226eab1834416bcab3e0d2910f7eb681
Callacos <callacos74@gmail.com>
2024-10-23 09:15:57 +0200

    3

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/**
+*main - Entry alphabet in lowercase
+*
+*Return : Alwayse 0 (success)
+*/
+
+int main(void)
+
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
