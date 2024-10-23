76567f5348f847ab376d4d7d5b518e6aab649d5b
Callacos <callacos74@gmail.com>
2024-10-23 20:09:28 +0200

    6

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * print_sign - Imprime le signe d'un nombre.
- * @n: Le nombre dont le signe sera vérifié.
- *
- * Description:
- * Cette fonction vérifie la valeur de l'entier `n` et imprime son signe.
- * - Si `n` est supérieur à zéro, elle imprime '+' et retourne 1.
- * - Si `n` est égal à zéro, elle imprime '0' et retourne 0.
- * - Si `n` est inférieur à zéro, elle imprime '-' et retourne -1.
- *
- * Return: 1 si `n` est positif, 0 si `n` est zéro, -1 si `n` est négatif.
- */
+* print_sign - Imprime le signe d'un nombre.
+* @n: Le nombre dont le signe sera vérifié.
+*
+* Description:
+* Cette fonction vérifie la valeur de l'entier `n` et imprime son signe.
+* - Si `n` est supérieur à zéro, elle imprime '+' et retourne 1.
+* - Si `n` est égal à zéro, elle imprime '0' et retourne 0.
+* - Si `n` est inférieur à zéro, elle imprime '-' et retourne -1.
+*
+* Return: 1 si `n` est positif, 0 si `n` est zéro, -1 si `n` est négatif.
+*/
 int print_sign(int n)
 {
 	if (n > 0)
@@ -25,7 +25,7 @@ int print_sign(int n)
 	}
 	else
 	{
-	_putchar('0');
+	_putchar('-');
 	return (-1);
 	}
 }
