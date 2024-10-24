1be2bdb690e18f32882148a2054e0516ee65ecc6
Callacos <callacos74@gmail.com>
2024-10-24 19:44:04 +0200

    12

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,53 +1,37 @@
-#include"main.h"
-#include<stdio.h>
+#include "main.h"
 
 /**
  * print_number - Prints a number.
  * @n: The number to print.
- *
- * Description: Converts an integer to its character representation
- * and outputs each digit using _putchar.
  */
 void print_number(int n)
 {
-	if (n >= 10)
+	if (n < 0)
 	{
-		_putchar((n / 10) + '0');
+	_putchar('-');
+	n = -n;
 	}
+
+	if (n / 10)
+	print_number(n / 10);
+
 	_putchar((n % 10) + '0');
-	}
+}
+
 /**
- * print_to_98 - Prints all natural numbers from n to 98,
- * followed by a new line.
- * @n: The starting number to print from.
- *
- * Description: Numbers are printed in order, separated by a comma and
- * a space. The sequence ends at 98.
+ * print_to_98 - Prints all natural numbers from n to 98.
+ * @n: The starting number.
  */
 void print_to_98(int n)
+{
+	while (n != 98)
 	{
-	if (n <= 98)
-	{
-		while (n < 98)
-		{
-			print_number(n);
-			_putchar(',');
-			_putchar(' ');
-			n++;
-		}
-		}
-		else
-		{
-		while (n > 98)
-		{
-			print_number(n);
-			_putchar(',');
-			_putchar(' ');
-			n--;
-		}
+	print_number(n);
+	_putchar(',');
+	_putchar(' ');
+	n += (n < 98) ? 1 : -1;
 	}
-	_putchar('9');
-	_putchar('8');
+	print_number(98);
 	_putchar('\n');
 }
 
