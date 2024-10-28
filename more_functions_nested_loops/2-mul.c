f9daf067995a99768603d4a5a931d28b9a51415c
Callacos <callacos74@gmail.com>
2024-10-28 10:20:13 +0100

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*mul - Multiplie 2 entiers
+*@a: le premier entier
+*@b: le deuxième entier
+*
+*description: cette fonction prend deux entiers en paramètres
+*et retourne le produit de ces deux entiers.
+*
+*Return: le produit de a et b
+*/
+int mul(int a, int b)
+	{
+	return (a * b);
+	}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 
+int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
 
