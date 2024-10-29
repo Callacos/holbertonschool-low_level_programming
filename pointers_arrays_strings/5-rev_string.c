d544e9b0c34a93f441ef318ace8c83260207b601
Callacos <callacos74@gmail.com>
2024-10-29 11:54:36 +0100

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * reverse_string - Inverse une chaîne de caractères.
+ * @s: Pointeur vers la chaîne de caractères à inverser.
+ *
+ * Description: Cette fonction inverse la chaîne `s` en permutant
+ * les caractères depuis le début jusqu'à la fin de la chaîne
+ * jusqu'à atteindre le milieu.
+ */
+void rev_string(char *s)
+{
+	int start = 0;
+	int end = 0;
+	char temp;
+
+	while (s[end] != '\0')
+	{
+		end++;
+	}
+	end--;
+
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
