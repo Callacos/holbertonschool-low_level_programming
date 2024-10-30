9e6a4cb1abf7bbe3ee9912e36051348affe8c085
Callacos <callacos74@gmail.com>
2024-10-30 09:31:34 +0100

    100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <limits.h>
 /**
  * _atoi - Convertit une chaîne en entier
  * @s: Chaîne de caractères à convertir
@@ -9,23 +9,31 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	int result = 0;
+	unsigned int result = 0;
 	int found_digit = 0;
 
 	while (*s)
 	{
 		if (*s == '-')
 			sign *= -1;
-		else if (*s == '+')
-			sign *= 1;
 		else if (*s >= '0' && *s <= '9')
 		{
-			found_digit = 1;
-			result = result * 10 + (*s - '0');
+		found_digit = 1;
+		if (result > INT_MAX / 10 || (result == INT_MAX
+				/ 10 && *s - '0' > INT_MAX % 10))
+		{
+		return ((sign == 1) ? INT_MAX : INT_MIN);
 		}
-		else if (found_digit)
-			break;
-		s++;
+		result = result * 10 + (*s - '0');
+		}
+	else if (found_digit)
+		break;
+	s++;
 	}
+
+
+	if (sign == -1 && result > (unsigned int)INT_MAX + 1)
+	return (INT_MIN);
+
 	return (sign * result);
 }
