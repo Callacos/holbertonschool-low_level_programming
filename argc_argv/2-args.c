fcd6778fe082288cb47f8c232ea4049128c53651
Callacos <callacos74@gmail.com>
2024-11-07 16:21:46 +0100

    2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - affiche le nom du programme suivi d'un retour à la ligne
+ * @argc: nombre d'arguments passés au programme
+ * @argv: tableau contenant les arguments passés au programme
+ *
+ * Return: (0) pour indiquer le succès
+ */
+int main(int argc, char *argv[])
+{
+
+	int i;
+
+	for ( i = 0; i < argc; i++)
+		{
+		printf("%s\n", argv[i]);
+		}
+
+	return (0);
+}
+
+
