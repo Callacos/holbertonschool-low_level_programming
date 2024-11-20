74c742ff13a7dbd735d61cd4f995e6f57d52afc4
Callacos <callacos74@gmail.com>
2024-11-20 18:35:31 +0100

    3function_pointers

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,88 +1,61 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
+
 /**
-*op_add - returns the sum of a and b
-*@a: dividende
-*@b: diviseur
-*
-*Return: return final resultat
-*/
+ * op_add - returns the sum of a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
 int op_add(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a + b);
 }
+
 /**
  * op_sub - returns the difference of a and b
- * @a: the first number
- * @b: the second number
+ * @a: first number
+ * @b: second number
  *
- * Return: the difference of a and b
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a - b);
 }
+
 /**
  * op_mul - returns the product of a and b
- * @a: the first number
- * @b: the second number
+ * @a: first number
+ * @b: second number
  *
- * Return: the product of a and b
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a * b);
 }
+
 /**
  * op_div - returns the result of the division of a by b
- * @a: the dividend
- * @b: the divisor
+ * @a: dividend
+ * @b: divisor
  *
- * Return: the result of the division, or exits if division by zero
+ * Return: result of the division of a by b
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit (0);
-	}
-
 	return (a / b);
 }
+
 /**
  * op_mod - returns the remainder of the division of a by b
- * @a: the dividend
- * @b: the divisor
+ * @a: dividend
+ * @b: divisor
  *
- * Return: the remainder of the division, or exits if division by zero
+ * Return: remainder of the division of a by b
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a % b);
 }
