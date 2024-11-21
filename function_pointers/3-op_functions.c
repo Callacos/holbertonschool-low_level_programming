a0040078d41ee2bf27ba029a1277e35843c9114c
Callacos <callacos74@gmail.com>
2024-11-21 09:57:12 +0100

    3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,23 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
+
 /**
-*op_add - returns the sum of a and b
-*@a: dividende
-*@b: diviseur
-*
-*Return: return final resultat
-*/
+ * op_add - returns the sum of a and b
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
 int op_add(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a + b);
 }
+
 /**
  * op_sub - returns the difference of a and b
  * @a: the first number
@@ -27,62 +21,42 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a - b);
 }
+
 /**
  * op_mul - returns the product of a and b
  * @a: the first number
  * @b: the second number
  *
  * Return: the product of a and b
-*/
+ */
 int op_mul(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a * b);
 }
+
 /**
  * op_div - returns the result of the division of a by b
  * @a: the dividend
  * @b: the divisor
  *
- * Return: the result of the division, or exits if division by zero
+ * Return: the result of the division
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a / b);
 }
+
 /**
  * op_mod - returns the remainder of the division of a by b
  * @a: the dividend
  * @b: the divisor
  *
- * Return: the remainder of the division, or exits if division by zero
+ * Return: the remainder of the division
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error: Division by zero is not allowed.\n");
-	exit(0);
-	}
-
 	return (a % b);
 }
+
