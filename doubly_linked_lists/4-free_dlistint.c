c960dc8c83642acd07813135dffc00517572ea99
Callacos <callacos74@gmail.com>
2024-12-03 14:13:27 +0100

    Add function to free a dlistint_t list and update header file

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the dlistint_t list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+
+	}
+
+}
