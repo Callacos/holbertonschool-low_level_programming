72c3d5f196e94bba7e8a858192ef5bc7918a29cf
Callacos <callacos74@gmail.com>
2024-12-03 09:51:41 +0100

    Add doubly linked list structure and print function

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+#include <stddef.h>
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the dlistint_t list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
