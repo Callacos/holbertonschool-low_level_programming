0e79f0264717a82ef08e545e2b20afaedfdf61d9
Callacos <callacos74@gmail.com>
2024-12-08 18:57:58 +0100

    Refactor file copy functionality for improved error handling and code organization

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,44 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
-#define ERROR(msg, file) (dprintf(2, msg, file), exit(1))
+#include <fcntl.h>
+
 /**
- * main - Copy the content of a file to another file
- * @ac: Number of arguments
- * @av: Array of argument strings
- * Return: 0 on success, exit codes on failure
+ * handle_close - Closes a file descriptor and handles
+ *  errors if the operation fails.
+ * @fd: The file descriptor to be closed.
  */
-int main(int ac, char **av)
+void handle_close(int fd)
 {
-	int fds[2], n;
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * open_file_for_reading - Opens a file for reading
+ * and handles errors if the operation fails.
+ * @filename: The name of the file to be opened.
+ * Return: The file descriptor for the opened file.
+ */
+int open_file_for_reading(const char *filename)
+{
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * open_file_for_writing - Opens a file for writing
+ * and handles errors if the operation fails.
+ * @filename: The name of the file to be opened.
+ * Return: The file descriptor for the opened file.
+ */
+int open_file_for_writing(const char *filename)
+{
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
+ * copy_file - Reads from a source file and writes
+ *  its content to a destination file.
+ * @fd_from: The file descriptor for the source file.
+ * @fd_to: The file descriptor for the destination file.
+ */
+void copy_file(int fd_from, int fd_to)
+{
+	ssize_t read_bytes, write_bytes;
 	char buffer[1024];
 
-	if (ac != 3)
-		ERROR("Usage: %s file_from file_to\n", av[0]);
-	fds[0] = open(av[1], O_RDONLY);
-
-	if (fds[0] == -1)
-		ERROR("Error: Can't read from %s\n", av[1]);
-	fds[1] = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (fds[1] == -1)
-		close(fds[0]), ERROR("Error: Can't write to %s\n", av[2]);
-
-	while ((n = read(fds[0], buffer, sizeof(buffer))) > 0)
+	while ((read_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		if (write(fds[1], buffer, n) == -1 || n != write(fds[1], buffer, n))
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes != read_bytes)
 		{
-			close(fds[0]);
-			close(fds[1]);
-			ERROR("Error: can't write to %s\n", av[2]);
+			handle_close(fd_from);
+			handle_close(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+			exit(99);
 		}
 	}
-	if (n == -1)
-		close(fds[0]), close(fds[1]), ERROR("Error: Can't read from %s\n", av[1]);
+	if (read_bytes == -1)
+	{
+		handle_close(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
+		exit(98);
+	}
+	if (read_bytes == 0)
+	{
+		char newline = '\n';
 
-	if (close(fds[0]) == -1 || close(fds[1]) == -1)
-		ERROR("Error: Can't close fd %d\n", fds[0] == -1 ? fds[1] : fds[0]);
+		write(fd_to, &newline, 1);
+	}
+}
+
+/**
+ * main - Copies content from one file to another.
+ * @ac: Argument count.
+ * @av: Argument vector (array of argument strings).
+ * Return: 0 on success, various error codes on failure.
+ */
+int main(int ac, char *av[])
+{
+		int fd_from;
+		int fd_to;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open_file_for_reading(av[1]);
+	fd_to = open_file_for_writing(av[2]);
+
+	copy_file(fd_from, fd_to);
+
+	char newline = '\n';
+
+	if (write(fd_to, &newline, 1) == -1)
+	{
+		handle_close(fd_from);
+		handle_close(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't write newline to %s\n", av[2]);
+		exit(99);
+	}
+
+	handle_close(fd_from);
+	handle_close(fd_to);
+
 	return (0);
 }
